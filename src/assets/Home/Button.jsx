d0221f45191a38161d0a7dad3d8ee6d52bdfdc73
Dev-id-A <davidaranzana618@gmail.com>
2025-08-22 10:54:37 +0200

    Languaje icons added

--- src/assets/Home/Button.jsx
+++ src/assets/Home/Button.jsx
@@ -1,9 +1,7 @@
-import { Link } from 'react-router'
-
-function Button({to, children, styleBtn}) {
+function Button({src}) {
   return (
-    <button className={`${styleBtn} border-[rgb(212,175,55)] border-2 h-20 w-40 rounded-3xl text-[rgb(212,175,55)] text-2xl`}>
-      <Link to={to}>{children}</Link>
+    <button className="size-10 cursor-pointer">
+      <img src={src} alt="Uk icon" />
     </button>
   )
 }
--- src/assets/Home/Navbar.jsx
+++ src/assets/Home/Navbar.jsx
@@ -1,7 +1,10 @@
+import Button from "./Button"
+
 function Navbar() {
   return (
-    <div className="h-25">
-      
+    <div className="h-25 flex items-center px-10 gap-2">
+      <Button src={"/spain.svg"} />
+      <Button src={"/uk.svg"} />
     </div>
   )
 }
