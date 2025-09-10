91a4cb0473a95dea3393ee52b38a88331e52a007
Dev-id-A <davidaranzana618@gmail.com>
2025-09-10 10:33:08 +0200

    Menu page details and background

--- pages/Menu.jsx
+++ pages/Menu.jsx
@@ -4,10 +4,18 @@ import menuJson from "../src/assets/Json/MenuJson";
 
 function Menu({lang}) {
   return (
-    <main className="w-screen h-screen overflow-hidden">
+    <main className="w-screen h-screen overflow-hidden bg-gradient-to-r from-[rgba(9,9,9,1)] via-[rgba(30,18,0,1)] to-[rgba(9,9,9,1)]">
       <div className="relative w-full h-30 flex flex-row justify-center items-center">
-        <Link to={"/"} className="absolute grid place-items-center border left-20 h-10 w-30">{menuJson.home[lang]}</Link>
-        <h1 className="text-6xl">{menuJson.presentation[lang]}</h1>
+
+        <Link to={"/"} className="absolute grid place-items-center left-20 h-10 w-30 text-white">
+        <svg className="absolute inset-0" viewBox="0 0 16 16" fill="currentColor">
+          <path d="M8 10L8 14L6 14L-2.62268e-07 8L6 2L8 2L8 6L16 6L16 10L8 10Z"></path>
+        </svg>
+          <p className="absolute top-11 left-15 text-black text-xl">{menuJson.home[lang]}</p>
+        </Link>
+
+        <h1 className="text-6xl text-white">{menuJson.presentation[lang]}</h1>
+
       </div>
       
       <div className="py-10 h-[90vh]">
--- src/assets/Menu.jsx/MenuBtn.jsx
+++ src/assets/Menu.jsx/MenuBtn.jsx
@@ -1,12 +1,12 @@
 function MenuBtn({children, image}) {
   return (
-    <button className="relative border size-full hover:cursor-pointer group">
+    <button className="relative border size-full hover:cursor-pointer group border-white">
       
       <img src={`${image}`} className="absolute inset-0 w-full h-full group-hover:scale-102 transition-all duration-1000" />
       
     <div className="absolute inset-0 bg-black/60 group-hover:bg-black/10 group-hover:scale-102 transition-all duration-1000"></div>
 
-      <h2 className="text-5xl text-white absolute inset-0 flex justify-center z-10">{children}</h2>
+      <h2 className="text-5xl text-white absolute inset-0 top-5 flex justify-center z-10 group-hover:scale-102 transition-all duration-1000">{children}</h2>
       
     </button>
   )
