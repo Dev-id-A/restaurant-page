61cedd520628c2c7011f2c139ac04f8c7034081e
Dev-id-A <davidaranzana618@gmail.com>
2025-09-10 12:19:50 +0200

    Presentation page changed

--- src/assets/Home/Presentation.jsx
+++ src/assets/Home/Presentation.jsx
@@ -3,9 +3,15 @@ import homeLang from "../Json/HomeJson"
 function Presentation({lang}) {
 
   return (
-    <main className="flex flex-col justify-center items-center py-20 font-yrsa text-center text-royal">
-      <h1 className="text-5xl">{homeLang.about[lang]}</h1>
+    <main className="h-screen w-screen flex flex-col justify-center items-center py-5 font-yrsa text-center text-royal">
+
+      <img src="/img/home/separador.png" alt="separator" className="h-auto w-[60vh] rotate-x-180"/>
+      
+        <h1 className="text-5xl">{homeLang.about[lang]}</h1>
         <p className="text-4xl mt-20 px-100">{homeLang.description[lang]}</p>
+
+      <img src="/img/home/separador.png" alt="separator" className="h-auto w-[60vh]"/>
+
     </main>
   )
 }
