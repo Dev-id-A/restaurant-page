09818e0c53754290fb66385b7baa0ae52eaf201b
Dev-id-A <davidaranzana618@gmail.com>
2025-09-11 18:29:06 +0200

    Centered reviews

--- src/assets/Home/Reviews.jsx
+++ src/assets/Home/Reviews.jsx
@@ -2,11 +2,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import ReviewsJson from "../Json/ReviewsJson";
+import { useState } from "react";
 
 
 function Reviews({lang}) {
+  const [actualIndex, setActualIndex] = useState(0);
+
   return (
-    <div className="flex justify-center text-royal w-full mt-20 h-100">
+    <div className="text-royal w-full h-100">
         <Swiper
         modules={[Autoplay]}
         loop
@@ -14,14 +17,18 @@ function Reviews({lang}) {
         slidesPerView={3}
         spaceBetween={50}
         speed={1500}
-        autoplay={{delay: 5000, disableOnInteraction:false}}
-        className="flex w-full h-full"
+        autoplay={{delay: 5000, disableOnInteraction: false}}
+        className="w-full h-full"
+        onSlideChange={(swiper)=> setActualIndex(swiper.realIndex)}
         >
           {ReviewsJson.map((review, index)=>(
             <SwiperSlide key={index}>
-              <div className="flex flex-col gap-5 text-center justify-center border-3 rounded-xl h-full p-10">
-                <h1>{review[lang]}</h1>
-                <h2>-{review.author}</h2>
+              <div className="h-full flex items-center justify-center">
+                <div className={`flex flex-col justify-center border-3 text-center rounded-xl p-10 gap-5 animation-all duration-1000 
+                  ${actualIndex === index ? "h-full":"size-50"}`}>
+                  <h1 className={`${actualIndex === index ? "opacity-100":"opacity-0"}`}>{review[lang]}</h1>
+                  <h2 className={`${actualIndex === index ? "opacity-100":"opacity-0"}`}>-{review.author}</h2>
+                </div>
               </div>
             </SwiperSlide>
           ))}
