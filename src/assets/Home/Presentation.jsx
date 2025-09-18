8978a854baecd3044bc116fb6044039532804fd1
Dev-id-A <davidaranzana618@gmail.com>
2025-09-18 18:22:52 +0200

    Presentation styling changes

--- src/assets/Home/Presentation.jsx
+++ src/assets/Home/Presentation.jsx
@@ -3,12 +3,12 @@ import homeLang from "../Json/HomeJson"
 function Presentation({lang}) {
 
   return (
-    <main className="h-screen w-auto flex flex-col justify-center items-center py-5 font-yrsa text-center text-royal">
+    <main className="flex flex-col gap-10 h-screen w-auto flex flex-col justify-center items-center py-5 font-yrsa text-center text-royal">
 
       <img src="/img/home/separador.png" alt="separator" className="h-auto w-[60vh] rotate-x-180"/>
       
-        <h1 className="text-5xl">{homeLang.about[lang]}</h1>
-        <p className="text-4xl mt-20 px-100">{homeLang.description[lang]}</p>
+        <h1 className="text-6xl font-teko">{homeLang.about[lang]}</h1>
+        <p className="text-4xl italic mt-3 px-70 font-luxurious">{homeLang.description[lang]}</p>
 
       <img src="/img/home/separador.png" alt="separator" className="h-auto w-[60vh]"/>
 
