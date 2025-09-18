57cef4be3dc0162e3ccd034af2dca07c58cfa3d4
Dev-id-A <davidaranzana618@gmail.com>
2025-09-18 16:56:50 +0200

    Accordion fixed

--- src/assets/FAQ/FAQAccordion.jsx
+++ src/assets/FAQ/FAQAccordion.jsx
@@ -1,25 +1,22 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import faq from "../Json/FAQJson"
 import { ChevronDownIcon } from "lucide-react"
 
 function FAQAccordion({lang}) {
   return (
-    <div className="w-full p-6 flex justify-center text-2xl">
+    <div className="w-full p-6 flex justify-center">
       <div className="w-1/2">
-        <Accordion
-                  
+        <Accordion   
           type="single"
-          
+          collapsible
           className="w-full"
-          defaultValue="item-1"
         >
           {faq.map((accordion, i)=> (
-            <AccordionItem className="border-3" value={`item-${i}`} key={i}>
-              <AccordionTrigger className="flex flex-row justify-between items-center w-full hover:cursor-pointer py-3">
+            <AccordionItem className="py-1 border-b-3 border-royal" value={`item-${i}`} key={i}>
+              <AccordionTrigger className="text-xl">
                 {accordion.ask[lang]}
-                <ChevronDownIcon/>
               </AccordionTrigger>
-              <AccordionContent className="px-5">{accordion.answer[lang]}</AccordionContent>
+              <AccordionContent className="px-5 text-lg">{accordion.answer[lang]}</AccordionContent>
             </AccordionItem>
           ))}
     </Accordion>
