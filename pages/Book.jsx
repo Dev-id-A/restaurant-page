872ebfdcff2da92bfa3deace9551f10f1ef0d7cf
Dev-id-A <davidaranzana618@gmail.com>
2025-09-27 11:52:56 +0200

    Book section logic finished

--- pages/Book.jsx
+++ pages/Book.jsx
@@ -5,6 +5,14 @@ import book from "../src/assets/Json/BookJson"
 import { useState } from "react"
 
 function Book({lang}) {
+    const today = new Date();
+    const maxDate = new Date();
+    maxDate.setDate(today.getDate() + 30);
+
+    const inputToday = today.toISOString().split("T")[0];
+    const inputMaxDate = maxDate.toISOString().split("T")[0];
+
+
     let [guest, setGuest] = useState(1);
     let [time, setTime] = useState(14);
 
@@ -16,7 +24,7 @@ function Book({lang}) {
 
         <h1>{book.bookTitle[lang]}</h1>
 
-      <div className="flex flex-col gap-20 mt-10 py-10 px-20 border-3 border-royal bg-[rgb(56,56,56)]">
+      <div className="flex flex-col gap-15 mt-10 py-10 px-20 border-3 border-royal bg-[rgb(56,56,56)]">
 
         <div className="flex flex-col justify-center items-center gap-3">
           <h2 className="text-5xl text-center">{book.guest[lang]}</h2>
@@ -25,7 +33,7 @@ function Book({lang}) {
 
         <div className="flex flex-col justify-center items-center gap-3">
           <h2 className="text-5xl text-center">{book.date[lang]}</h2>
-          <input type="date" defaultValue={new Date().toISOString().split("T")[0]} className="border-3 border-royal"/>
+          <input type="date" defaultValue={inputToday} min={inputToday} max={inputMaxDate} className="border-3 border-royal"/>
         </div>
         
         <div className="flex flex-col justify-center items-center gap-3">
@@ -33,8 +41,8 @@ function Book({lang}) {
           <BookSection parameter={time} {...{guest, setGuest, time, setTime}}></BookSection>
         </div>
         
-
-        <NumberBtn size={"px-10 py-5"} children={`${book.bookBnt[lang]}`}/>
+        <button className="bg-black flex justify-center items-center border-3 border-royal hover:cursor-pointer px-10 py-5">
+          <a href="mailto:dev.id.a.contact@gmail.com" target="_blank">{book.bookBnt[lang]}</a></button>
 
       </div>
       
