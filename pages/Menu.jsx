4aae8376c32a8a6fd4258fe4015556e121f3310a
Dev-id-A <davidaranzana618@gmail.com>
2025-10-01 20:18:36 +0200

    Menu scroll bug fixed

--- pages/Menu.jsx
+++ pages/Menu.jsx
@@ -1,11 +1,12 @@
 import MenuSection from "../src/assets/Menu/MenuSection";
 import menuJson from "../src/assets/Json/MenuJson";
 import LinkBtn from "../src/assets/FAQ/LinkBtn";
+import { useEffect } from "react";
 
 function Menu({lang}) {
+  useEffect(()=> window.scrollTo(0,0));
   return (
-    <main className="flex flex-col bg-black text-royal min-h-screen 
-    md:overflow-hidden">
+    <main className="flex flex-col bg-black text-royal min-h-screen font-luxurious">
 
       <div className="flex flex-col justify-center items-center">
         <LinkBtn {...{lang}}/>
